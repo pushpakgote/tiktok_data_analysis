f676737722dd9d0ebbdabd94ef6a58223ee09263
Pushpak Gote <pushpakgote25@gmail.com>
2022-09-21 13:08:50 +0000

    terminal in browser

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ hashtag=st.text_input('Search hashtags . . .',value="")
 
 if st.button('Get Data'):
     st.write(hashtag)
-    call(['python','tiktok.py',hashtag])
+    call(hashtag.split(' '))
+    #call(['python','tiktok.py',hashtag])
     with open('test.txt') as f:
         contents = f.read()
     st.write(contents)
