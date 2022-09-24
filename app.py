591a5c8682d811f249c213b6641763e2ba0a8bf1
Pushpak Gote <pushpakgote25@gmail.com>
2022-09-24 20:24:37 +0000

    .

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ if st.button('Get Data'):
     
     #Left chart : Video stats
     scatter1=px.scatter(df,x='stats_shareCount',y='stats_commentCount')
-    update_layout( xaxis_title="Shares",yaxis_title="Comments" )
+    scatter1.update_layout( xaxis_title="Shares",yaxis_title="Comments" )
     left_col.plotly_chart(scatter1,use_container_width=True)
     
     #Show tabular data
