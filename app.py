ebff1e0ce7e06c3e6f082654a823239be10842a5
Pushpak Gote <pushpakgote25@gmail.com>
2022-09-26 13:07:43 +0000

    .

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ with st.sidebar:
 st.sidebar.markdown("<div> <h1 style='display:inline-block;text-align:center;'>TikTok Analytics</h1> </div> ",unsafe_allow_html=True)
 st.sidebar.markdown("This dashboard allows you to analyze latest tiktok videos using Python and Streamlit")
 #st.sidebar.markdown("Here's how it works : <ol><li>1. Enter <i>hashtag></i> you wish to analyze</li><li>2. Hit <i>Get Data</i> </li> <li>3. Analyze data with charts </li>",unsafe_allow_html=True)
-st.sidebar.markdown("Here's how it works : <br>1. Enter <i>hashtag></i> you wish to analyze <br> 2. Hit <i>Get Data</i> <br>3. Analyze data with charts ",unsafe_allow_html=True)
+st.sidebar.markdown("Here's how it works : <br>1. Enter <i>hashtag</i> you wish to analyze <br> 2. Hit <i>Get Data</i> <br>3. Analyze data with charts ",unsafe_allow_html=True)
 
 
 #input
@@ -76,9 +76,32 @@ if st.button('Get Data'):
     df[bool_cols]=df[bool_cols].astype('str') 
     
     #Plotting histogram
-    fig=px.histogram(df,x='desc',y='authorStats_diggCount',hover_data=['desc'],height=300)
-    fig.update_layout( yaxis_title="Likes" )
+    fig=px.histogram(df,x='author_uniqueId',y='authorStats_diggCount',hover_data=['desc'],height=300)
+    fig.update_layout( xaxis_title="Account (@)",yaxis_title="Likes" )
     st.plotly_chart(fig,use_container_width=True)
+    
+    #Top 3 videos video_id
+    #top_3_video_id=[0,0,0]
+    #top_3_like_isto_views_score=[]
+    #for i in range(df.shape[0]):
+    top_3=pd.DataFrame()
+    top_3['likes_to_views']=df.stats_diggCount / df.stats_playCount
+    top_3['video_id']=df.video_id
+    top_3=top_3.sort_values(by='likes_to_views',ascending=False).iloc[:3]  
+    
+    #show in site
+    top_3
+    
+    cols=st.columns(3)
+    for i,col in enumerate(cols):
+        col.header(df['author_nickname'][df['video_id']==top_3.loc[i,'video_id']])
+        col.write("Account (@) : {}".fomat(df['author_uniqueId'][df['video_id']==top_3.loc[i,'video_id']]))
+        col.write("Video description : {}".fomat(df['desc'][df['video_id']==top_3.loc[i,'video_id']]))
+        col.write("Views : {}".format(df['stats_playCount'][df['video_id']==top_3.loc[i,'video_id']]))
+        col.write("Likes : {}".format(df['stats_diggCount'][df['video_id']==top_3.loc[i,'video_id']]))
+        col.write("Comments : {}".format(df['stats_commentCount'][df['video_id']==top_3.loc[i,'video_id']]))
+        col.write("Shares : {}".format(df['stats_shareCount'][df['video_id']==top_3.loc[i,'video_id']]))
+    
     
     #Split Columns
     left_col,right_col=st.columns(2)
