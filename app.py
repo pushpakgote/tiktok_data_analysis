2b8e8e42b413fbe5c2ad20755694d80bcf7b00b5
Pushpak Gote <pushpakgote25@gmail.com>
2022-09-27 12:12:19 +0000

    .

--- app.py
+++ app.py
@@ -122,9 +122,9 @@ if st.button('Get Data'):
         
             for i,col in enumerate(cols):
                 col.header( (df['author_nickname'][df['video_id']==top_3.loc[i,'video_id']]).values[0]  )
-                col.write("Engagement rate : {} %".format( top_3.loc[i,'tiktok_engagement_rate'] ) )
                 col.write("username (@) : {}".format( (df['author_uniqueId'][df['video_id']==top_3.loc[i,'video_id']]).values[0] ))
                 col.write("Video description : {}".format( (df['desc'][df['video_id']==top_3.loc[i,'video_id']]).values[0] ))
+                col.write("Engagement rate : {} %".format( top_3.loc[i,'tiktok_engagement_rate'] ) )
                 col.write("Views : {}".format( (df['stats_playCount'][df['video_id']==top_3.loc[i,'video_id']]).values[0] )) 
                 col.write("Likes : {}".format( (df['stats_diggCount'][df['video_id']==top_3.loc[i,'video_id']]).values[0] ))
                 col.write("Comments : {}".format( (df['stats_commentCount'][df['video_id']==top_3.loc[i,'video_id']]).values[0] ))
