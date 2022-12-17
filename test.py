71e9b1db8984d3c29e03dcc8ba4e9a9786c92830
Pushpak Gote <96307489+pushpakgote@users.noreply.github.com>
2022-12-17 12:37:43 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ with TikTokApi() as api:
     print("inside 1st loop")
     #for trending_video in api.trending.videos(count=20):
     tag=api.hashtag("hello")
-    for v in tag.videos():
+    for v in api.hashtag("hello").videos():
         print("in v loop")
-        print(v.as_dict)
+        print(v)
     print("videos: ",tag.videos())
     print("dict: ",tag.as_dict)
     print(tag)
