92fccaab90d1ec9c560f55f084beb00f2a532a4c
Pushpak Gote <96307489+pushpakgote@users.noreply.github.com>
2022-12-17 11:36:41 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from TikTokApi import TikTokApi
 
+print("inside test.py")
 # Watch https://www.youtube.com/watch?v=-uCt1x8kINQ for a brief setup tutorial
 with TikTokApi() as api:
+    print("inside 1st loop")
     for trending_video in api.trending.videos(count=20):
         # Prints the author's username of the trending video.
         print(trending_video.author.username)
