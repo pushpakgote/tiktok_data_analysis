8efa7a8e638411931680f23b0d0f664b84223d1c
Pushpak Gote <96307489+pushpakgote@users.noreply.github.com>
2022-12-17 12:08:32 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ print("inside test.py")
 # Watch https://www.youtube.com/watch?v=-uCt1x8kINQ for a brief setup tutorial
 with TikTokApi() as api:
     print("inside 1st loop")
-    for trending_video in api.trending.videos(count=20):
+    #for trending_video in api.trending.videos(count=20):
+    for trending_video in api.hashtag("hello"):
         # Prints the author's username of the trending video.
-        print(trending_video.author.username)
+        print(trending_video)
